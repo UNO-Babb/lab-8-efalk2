1501321615e2036a8f42470703d8f4cb9fe602b9
efalk2 <efalk@unomaha.edu>
2025-04-02 14:39:10 +0000

    some changes

--- ProcessData.py
+++ ProcessData.py
@@ -1,9 +1,15 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Ella Falk
+#Date: 4/2/25
+#Assignment: Lab 8
 
-import random
+def makeID(first, last, num):
+  while len(last) < 5:
+    last = last + "x"
+
+  last3 = num[-3:]
+  id = first[0] + last + last3
+  return id
 
 def main():
 
@@ -12,7 +18,18 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  student = "Antwan Dougherty AntwanDougherty@yahoo.com 443-13-3556 03/28/1996 Freshman Philosophy"
+  studentData = student.split()
+  print(studentData)
+  firstName = studentData[0]
+  lastName = studentData[1]
+  studentID = studentData[3]
+  year = studentData[5]
+  major = studentData[6]
+  userID = makeID(firstName, lastName, studentID)
 
+  studentOutput = lastName + ", " + firstName + ", " + userID + "\n"
+  outFile.write(studentOutput)
 
 
   #Close files in the end to save and ensure they are not damaged.
