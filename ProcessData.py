8e3ed67c80760dfe8fca46e833bcfceff2697b3c
efalk2 <efalk@unomaha.edu>
2025-04-02 14:55:12 +0000

    finished lab

--- ProcessData.py
+++ ProcessData.py
@@ -11,6 +11,24 @@ def makeID(first, last, num):
   id = first[0] + last + last3
   return id
 
+def makeMajorYear(major, year):
+  first3 = major[0:3]
+  year = year.upper()
+  yearAb = ""
+  if year == "FRESHMAN":
+    yearAb = "FR"
+  elif year == "SOPHOMORE":
+    yearAb = "SO"
+  elif year == "JUNIOR":
+    yearAb = "JR"
+  elif year == "SENIOR":
+    yearAb = "SR"
+  else:
+    yearAb = "GR"
+
+  majorYear = first3 + "-" + yearAb
+  return majorYear
+
 def main():
 
   #Open the files we will be using
@@ -18,18 +36,19 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
-  student = "Antwan Dougherty AntwanDougherty@yahoo.com 443-13-3556 03/28/1996 Freshman Philosophy"
-  studentData = student.split()
-  print(studentData)
-  firstName = studentData[0]
-  lastName = studentData[1]
-  studentID = studentData[3]
-  year = studentData[5]
-  major = studentData[6]
-  userID = makeID(firstName, lastName, studentID)
+  for student in inFile:
+    #student = "Antwan Dougherty AntwanDougherty@yahoo.com 443-13-3556 03/28/1996 Freshman Philosophy"
+    studentData = student.split()
+    firstName = studentData[0]
+    lastName = studentData[1]
+    studentID = studentData[3]
+    year = studentData[5]
+    major = studentData[6]
+    userID = makeID(firstName, lastName, studentID)
+    majorYear = makeMajorYear(major, year)
 
-  studentOutput = lastName + ", " + firstName + ", " + userID + "\n"
-  outFile.write(studentOutput)
+    studentOutput = lastName + "," + firstName + "," + userID + "," + majorYear + "\n"
+    outFile.write(studentOutput)
 
 
   #Close files in the end to save and ensure they are not damaged.
